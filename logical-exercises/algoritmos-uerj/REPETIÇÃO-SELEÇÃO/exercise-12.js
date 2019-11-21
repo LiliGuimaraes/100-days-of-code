f60800cd859ca9fd69fd3dff7d24b131c458802d
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-11-21 11:25:28 -0300

    REPETICOES - exercise-12.js

--- logical-exercises/algoritmos-uerj/REPETIÇÃO-SELEÇÃO/exercise-12.js
+++ logical-exercises/algoritmos-uerj/REPETIÇÃO-SELEÇÃO/exercise-12.js
@@ -5,18 +5,51 @@ const r1 = readline.createInterface({
   output: process.stdout
 });
 
+// @TODO: create async function to check invalid arguments
+// function fine(param1, param2) {
+//   if (
+//     (param1 && param2 == null) ||
+//     (param1 && param2 === undefined) ||
+//     (param1 && param2 == "") )
+
+//     throw new Error("Valores inválidos no sistema");
+// }
+
 r1.question(
   `Digite o número de gols marcados pelo GRÊMIO: `,
   userGolsGremio => {
-    r1.question(`Digite o número de gols marcados pelo INTER: `, userGolsInter => {
-      if(userGolsGremio === userGolsInter){
-        console.log("EMPATE!!")          
-      } else if(userGolsGremio > userGolsInter){
-        console.log("O vencedor é o GRÊMIO")  
-      } else{
-        console.log("O vencedor é o INTER")  
+    r1.question(
+      `Digite o número de gols marcados pelo INTER: `,
+      userGolsInter => {
+        if ((userGolsInter === null) || (userGolsGremio === null)){
+          r1.setPrompt(`Respostas inválidas para o sistema` );
+          r1.prompt();
+        } else {
+          if (userGolsGremio === userGolsInter) {
+            r1.setPrompt(`EMPATE!!`);
+            r1.prompt();
+            r1.question(`Novo jogo? 1-SIM, 2-NÃO`),
+              userResponde => {
+                console.log(userResponde);
+              };
+          } else if (userGolsGremio > userGolsInter) {
+            r1.setPrompt(`O vencedor é o GRÊMIO`);
+            r1.prompt();
+            r1.question(`Novo jogo? 1-SIM, 2-NÃO`),
+              userResponde => {
+                console.log(userResponde);
+              };
+          } else {
+            r1.setPrompt(`O vencedor é o INTER`);
+            r1.prompt();
+            r1.question(`Novo jogo? 1-SIM, 2-NÃO`),
+              userResponde => {
+                console.log(userResponde);
+              };
+          }
+        }
+        r1.close();
       }
-      r1.close();
-    })
-  });
-
+    );
+  }
+);
