00cd39908be8542cd1bbaddac6214ff9a07c986a
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-11-28 11:59:22 -0300

    URI JUDGE - 1234

--- logical-exercises/URI-JUDGE/STRINGS/SentencaDancante.py
+++ logical-exercises/URI-JUDGE/STRINGS/SentencaDancante.py
@@ -1,2 +1,20 @@
-text = input().lower()
-print text
+while True:
+    try:
+        linha_new = ""
+        linha = input()
+
+        maiuscula = True
+
+        for l in linha:
+            if l == ' ':
+                linha_new += ' '
+                continue
+            if maiuscula:
+                linha_new += l.upper()
+                maiuscula = False
+            else:
+                linha_new += l.lower()
+                maiuscula = True
+        print(linha_new)
+    except EOFError:
+        break
