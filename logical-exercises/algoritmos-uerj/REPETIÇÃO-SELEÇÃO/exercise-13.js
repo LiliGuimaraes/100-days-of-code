dc13c40055d1f0ba35bd2725bd40c1b11fa087de
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-12-04 13:00:08 -0300

    organize this study

---
+++ logical-exercises/algoritmos-uerj/REPETIÇÃO-SELEÇÃO/exercise-13.js
@@ -0,0 +1,23 @@
+const readline = require("readline")
+
+const readAnswers = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+ readAnswers.question(`Qual o nome da candidata?`, nameCandidate => {
+
+ }) != null ? readAnswers.console.log  :
+
+readAnswers.question( => {
+    readAnswers.question(`Qual a altura da candidata?`, higthCandidate => {
+      
+       }else if(nameCandidate === "FIM"){
+           readAnswers.setPrompt(`O nome da candidata mais alta é: ${nameTollerCandidate}` )
+       }{
+        
+       } 
+        readAnswers.close();
+    })
+})
--- recreating-sites/news-cardscss-only/README.markdown
+++
@@ -1,4 +0,0 @@
-# News Cards - CSS only
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/choogoor/pen/YWBxAg](https://codepen.io/choogoor/pen/YWBxAg).
-
- Pure CSS news cards with revealing content on hover. Please feel free to drop a comment with some suggestion for improvement. 
